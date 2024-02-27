d25da9c80e04c0786a654f29de3558e65acdce67
asciibits <34527442+asciibits@users.noreply.github.com>
2024-02-26 18:35:31 -0800

    Move dist to docs so github pages can serve it better

--- dist.sh
+++ dist.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-npx esbuild ./src/ui/index.ts ./src/ui/sidebar.ts ./src/ui/board_id_worker_entry.ts --bundle --minify --outdir=dist
-cp ./ui/icon.svg dist/
-cp ./ui/favicon.ico dist/
-npx css-minify -f ./ui/minestyle.css -o dist/
-mv dist/minestyle.min.css dist/minestyle.css
-npx html-minifier-terser --collapse-whitespace --remove-comments --minify-js true ./ui/minesweeper.html -o ./dist/index.html
-sed -i -e 's|\.\./lib/ui/|./|g' ./dist/minesweeper.html
+npx esbuild ./src/ui/index.ts ./src/ui/sidebar.ts ./src/ui/board_id_worker_entry.ts --bundle --minify --outdir=docs
+cp ./ui/icon.svg docs/
+cp ./ui/favicon.ico docs/
+npx css-minify -f ./ui/minestyle.css -o docs/
+mv docs/minestyle.min.css docs/minestyle.css
+npx html-minifier-terser --collapse-whitespace --remove-comments --minify-js true ./ui/minesweeper.html -o ./docs/index.html
+sed -i -e 's|\.\./lib/ui/|./|g' ./docs/minesweeper.html
 
 
-sed -i -e "s/['\"]<<BOARD_ID_WORKER_ENTRY>>[\'\"]/\n<<BOARD_ID_WORKER_ENTRY>>\n/" ./dist/index.js
+sed -i -e "s/['\"]<<BOARD_ID_WORKER_ENTRY>>[\'\"]/\n<<BOARD_ID_WORKER_ENTRY>>\n/" ./docs/index.js
 
 TMPFILE="$(mktemp)"
-cat ./dist/index.js | (
+cat ./docs/index.js | (
   while IFS= read -r line; do
   if [[ "${#line}" -lt 100 && "${line}" == "<<BOARD_ID_WORKER_ENTRY>>" ]]; then
     echo -n "'$(
-      sed -e s/"'"/'\\'"'"/g -e 's/\\/\\\\/g' ./dist/board_id_worker_entry.js |
+      sed -e s/"'"/'\\'"'"/g -e 's/\\/\\\\/g' ./docs/board_id_worker_entry.js |
       sed -e ':q;N;s/\n/\\n/g;t q;')'"
   else
     echo "${line}"
   fi
 done) > "$TMPFILE"
-mv "$TMPFILE" ./dist/index.js
+mv "$TMPFILE" ./docs/index.js
