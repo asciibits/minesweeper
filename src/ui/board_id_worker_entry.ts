b6b8532a67b0b05c1cb7964b270a8481d0c32c9f
asciibits <34527442+asciibits@users.noreply.github.com>
2024-03-17 15:44:13 -0700

    Start caching the previous board and view states.
    This makes encoding minor changes in the view state (or ellapsedTime)
    more efficient.

--- src/ui/board_id_worker_entry.ts
+++ src/ui/board_id_worker_entry.ts
@@ -4,17 +4,75 @@ import {
   MessageRequest,
 } from '../minesweeper/board_id_worker.js';
 import {
+  EncodedBoardState,
+  KnownBoardState,
+  KnownCell,
   assertBoardState,
   assertEncodedBoardState,
   decodeBoardState,
-  encodeBoardState,
+  encodeBoardId,
+  encodeElapsedTime,
+  encodeViewState,
 } from '../minesweeper/minesweeper_storage.js';
+
+class CachedDecoder {
+  // The decoded board state
+  private width?: number;
+  private height?: number;
+  private cellData?: KnownCell[];
+  private cachedElapsedTime?: number;
+
+  // The encoded board state
+  private boardId?: string;
+  private viewState?: string;
+  private elapsedTime?: string;
+
+  constructor() {}
+
+  encodeBoardState(boardState: KnownBoardState): EncodedBoardState {
+    const {width, height, cellData, elapsedTime: knownElapsedTime} = boardState;
+    let minesMatch = false;
+    let cellsMatch = false;
+    if (width === this.width && height === this.height) {
+      minesMatch = true;
+      cellsMatch = true;
+      const cachedCellData = this.cellData ?? [];
+      for (let i = 0; i < cellData.length; i++) {
+        if (cellData[i] !== cachedCellData[i]) {
+          cellsMatch = false;
+          if (cellData[i].isMine !== cachedCellData[i]?.isMine) {
+            minesMatch = false;
+            break;
+          }
+        }
+      }
+    }
+    const boardId = minesMatch ? this.boardId! : encodeBoardId(boardState);
+    const viewState = cellsMatch ? this.viewState : encodeViewState(boardState);
+    const elapsedTime =
+      knownElapsedTime === this.cachedElapsedTime
+        ? this.elapsedTime
+        : encodeElapsedTime(knownElapsedTime);
+
+    this.width = width;
+    this.height = height;
+    this.cellData = cellData;
+    this.cachedElapsedTime = knownElapsedTime;
+    this.boardId = boardId;
+    this.viewState = viewState;
+    this.elapsedTime = elapsedTime;
+
+    return {boardId, viewState, elapsedTime};
+  }
+}
 
 /**
  * window is undefined when this is loaded as a worker module, and onmessage is
  * undefined when loaded as a node test
  */
 if (typeof window === 'undefined' && typeof onmessage !== 'undefined') {
+  const cachedDecoder = new CachedDecoder();
+
   // setLoggingLevel(LoggingLevel.TRACE);
   onmessage = (e: MessageEvent) => {
     trace('Processing web worker event: %o', e);
@@ -23,7 +81,7 @@ if (typeof window === 'undefined' && typeof onmessage !== 'undefined') {
       case 'ENCODE': {
         const {boardState} = message;
         assertBoardState(boardState);
-        const encodedBoardState = encodeBoardState(boardState);
+        const encodedBoardState = cachedDecoder.encodeBoardState(boardState);
         postMessage({
           messageType: 'ENCODE',
           encodedBoardState,
